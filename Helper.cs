766eb1a0cff940231ca1a20e4c76baee689ca298
llewelon <llewelon@gmail.com>
2023-03-19 17:43:43 -0600

    Initial Commit

---
+++ Helper.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace SqlMaster
+{
+    public class Helper
+    {
+        public enum ConnectionType
+        {
+            Collections,
+            CollectionsRemote
+        }
+
+        #region List<SqlParameter>
+        public static DataRow GetFirstRowOfTable(string sql, ConnectionType connectionType, List<SqlParameter> parameters, bool isStoredProcedure = false)
+        {
+            DataTable dataTable = GetDataFromDatabase(sql, connectionType, parameters, isStoredProcedure);
+
+            if (dataTable.Rows.Count > 0)
+                return dataTable.Rows[0];
+
+            return null;
+        }
+
+        public static SqlParameter CreateSqlParameter(string parameterName, SqlDbType sqlDbType, int size, object value)
+        {
+            return new SqlParameter() { ParameterName = parameterName, SqlDbType = sqlDbType, Size = size, Value = value };
+        }
+
+        public static SqlParameter CreateSqlParameter(string parameterName, SqlDbType sqlDbType, object value)
+        {
+            return new SqlParameter() { ParameterName = parameterName, SqlDbType = sqlDbType, Value = value };
+        }
+
+        public static DataTable GetDataFromDatabase(string sql, ConnectionType connectionType, List<SqlParameter> parameters, bool isStoredProcedure = false)
+        {
+            DataTable dataTable = new DataTable();
+
+            GetDataFromDatabaseAppend(sql, connectionType, parameters, dataTable, isStoredProcedure);
+
+            return dataTable;
+        }
+
+        public static void GetDataFromDatabaseAppend(string sql, ConnectionType connectionType, List<SqlParameter> parameters, DataTable dataTable, bool isStoredProcedure = false)
+        {
+            using (SqlDataAdapter dataAdapter = new SqlDataAdapter(sql, GetConnectionString(connectionType)))
+            {
+                AddParameters(dataAdapter.SelectCommand, parameters);
+
+                SetCommandTypeIfStoredProcedure(dataAdapter.SelectCommand, isStoredProcedure);
+
+                SqlFillWithTimer(dataAdapter, dataTable);
+            }
+        }
+
+        private static void AddParameters(SqlCommand cmd, List<SqlParameter> parameters)
+        {
+            if (parameters != null)
+            {
+                foreach (SqlParameter parameter in parameters)
+                {
+                    cmd.Parameters.Add(parameter);
+                }
+            }
+        }
+
+        public static Object ExecuteScalar(string sql, ConnectionType connectionType, List<SqlParameter> parameters, bool isStoredProcedure = false)
+        {
+            using (SqlConnection connection = new SqlConnection(GetConnectionString(connectionType)))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                AddParameters(command, parameters);
+                SetCommandTypeIfStoredProcedure(command, isStoredProcedure);
+                try
+                {
+                    return command.ExecuteScalar();
+                }
+                catch (SqlException sqlException)
+                {
+                    return null;
+                }
+            }
+        }
+        #endregion List<SqlParameter>
+
+        #region Dictionary<string, object>
+        public static DataRow GetFirstRowOfTable(string sql, ConnectionType connectionType, Dictionary<string, object> parameters, bool isStoredProcedure = false)
+        {
+            DataTable dataTable = GetDataFromDatabase(sql, connectionType, parameters, isStoredProcedure);
+
+            if (dataTable.Rows.Count > 0)
+                return dataTable.Rows[0];
+
+            return null;
+        }
+
+        public static DataTable GetDataFromDatabase(string sql, ConnectionType connectionType, Dictionary<string, object> parameters, bool isStoredProcedure = false)
+        {
+            DataTable dataTable = new DataTable();
+
+            GetDataFromDatabaseAppend(sql, connectionType, parameters, dataTable, isStoredProcedure);
+
+            return dataTable;
+        }
+
+        public static void GetDataFromDatabaseAppend(string sql, ConnectionType connectionType, Dictionary<string, object> parameters, DataTable dataTable, bool isStoredProcedure = false)
+        {
+            using (SqlDataAdapter dataAdapter = new SqlDataAdapter(sql, GetConnectionString(connectionType)))
+            {
+                AddParameters(dataAdapter.SelectCommand, parameters);
+
+                SetCommandTypeIfStoredProcedure(dataAdapter.SelectCommand, isStoredProcedure);
+
+                SqlFillWithTimer(dataAdapter, dataTable);
+            }
+        }
+
+        private static void AddParameters(SqlCommand cmd, Dictionary<string, object> parameters)
+        {
+            if (parameters != null)
+            {
+                foreach (string key in parameters.Keys)
+                {
+                    cmd.Parameters.AddWithValue(key, parameters[key]);
+                }
+            }
+        }
+
+        public static Object ExecuteScalar(string sql, ConnectionType connectionType, Dictionary<string, object> parameters, bool isStoredProcedure = false)
+        {
+            using (SqlConnection connection = new SqlConnection(GetConnectionString(connectionType)))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                AddParameters(command, parameters);
+                SetCommandTypeIfStoredProcedure(command, isStoredProcedure);
+                return command.ExecuteScalar();
+            }
+        }
+        #endregion Dictionary<string, object>
+
+        #region no parameters
+        public static DataRow GetFirstRowOfTable(string sql, ConnectionType connectionType, bool isStoredProcedure = false)
+        {
+            DataTable dataTable = GetDataFromDatabase(sql, connectionType, isStoredProcedure);
+
+            if (dataTable.Rows.Count > 0)
+                return dataTable.Rows[0];
+
+            return null;
+        }
+
+        public static DataTable GetDataFromDatabase(string sql, ConnectionType connectionType, bool isStoredProcedure = false)
+        {
+            DataTable dataTable = new DataTable();
+
+            GetDataFromDatabaseAppend(sql, connectionType, dataTable, isStoredProcedure);
+
+            return dataTable;
+        }
+
+        public static void GetDataFromDatabaseAppend(string sql, ConnectionType connectionType, DataTable dataTable, bool isStoredProcedure = false)
+        {
+            using (SqlDataAdapter dataAdapter = new SqlDataAdapter(sql, GetConnectionString(connectionType)))
+            {
+                SetCommandTypeIfStoredProcedure(dataAdapter.SelectCommand, isStoredProcedure);
+
+                SqlFillWithTimer(dataAdapter, dataTable);
+            }
+        }
+
+        public static Object ExecuteScalar(string sql, ConnectionType connectionType, bool isStoredProcedure = false)
+        {
+            using (SqlConnection connection = new SqlConnection(GetConnectionString(connectionType)))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                SetCommandTypeIfStoredProcedure(command, isStoredProcedure);
+                return command.ExecuteScalar();
+            }
+        }
+        #endregion no parameters
+
+        public static void SqlFillWithTimer(SqlDataAdapter adapter, DataTable table)
+        {
+            //this has a timer
+
+            var sw = new Stopwatch();
+            sw.Start();
+            adapter.Fill(table);
+            sw.Stop();
+        }
+
+        public static int TableSizeInBytes(DataTable table)
+        {
+            int size = 0;
+
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    if (item is int)
+                        size += 4;
+                    else if (item is string)
+                        size += ((string)item).Length;
+                    else if (item is decimal)
+                        size += 16;
+                    else if (item is double)
+                        size += 8;
+                    else if (item is bool)
+                        size += 1;
+                    else if (item is DBNull)
+                        size += 1;
+                    else if (item is DateTime)
+                        size += 8;
+                    else if (item is Guid)
+                        size += 16;
+                    else if (item is Byte)
+                        size += 1;
+                    else if (item.GetType() == typeof(byte[]))
+                        size += ((byte[])item).Length;
+                    else
+                        throw new Exception("Unable to compute result size in UtilER.TableSizeInBytes().  Missing type: " + item.GetType());
+                }
+            }
+
+            return size;
+        }
+
+        private static void SetCommandTypeIfStoredProcedure(SqlCommand sqlCommand, bool isStoredProcedure)
+        {
+            if (isStoredProcedure)
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+        }
+
+        public static bool UpdateDatabase(string sql, ConnectionType connectionType, List<SqlParameter> parameters, bool isStoredProcedure = false)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(GetConnectionString(connectionType)))
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    AddParameters(command, parameters);
+                    SetCommandTypeIfStoredProcedure(command, isStoredProcedure);
+                    command.ExecuteNonQuery();
+
+                    return true;
+                }
+            }
+            catch (Exception exception)
+            {
+                return false;
+            }
+        }
+
+        public static int UpdateDatabase(string sql, ConnectionType connectionType, Dictionary<string, object> parameters, bool isStoredProcedure = false)
+        {
+            using (SqlConnection connection = new SqlConnection(GetConnectionString(connectionType)))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                AddParameters(command, parameters);
+                SetCommandTypeIfStoredProcedure(command, isStoredProcedure);
+                return command.ExecuteNonQuery();
+            }
+        }
+
+        public static string GetConnectionString(ConnectionType connectionType)
+        {
+            switch (connectionType)
+            {
+                case ConnectionType.Collections:
+                    return GetCollectionsConnectionString();
+                case ConnectionType.CollectionsRemote:
+                    return GetCollectionsLaptopConnectionString();
+                default:
+                    return String.Empty;
+            }
+        }
+
+        public static string GetCollectionsConnectionString()
+        {
+            return new SqlConnectionStringBuilder()
+            {
+                DataSource = "WEAPONX\\MYCOLLECTIONS",
+                InitialCatalog = "Collections",
+                UserID = "CollectionsUser",
+                Password = "Otsuka@1"
+            }.ConnectionString;
+        }
+
+        public static string GetCollectionsLaptopConnectionString()
+        {
+            return new SqlConnectionStringBuilder()
+            {
+                DataSource = "tcp:WeaponX,49172",
+                InitialCatalog = "Collections",
+                UserID = "CollectionsUser",
+                Password = "Otsuka@1"
+            }.ConnectionString;
+        }
+    }
+}
---
+++ Parameters.cs
@@ -0,0 +1,161 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace SqlMaster
+{
+    public static class Parameters
+    {
+        public static class Client
+        {
+            public static SqlParameter ClientIdParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@ClientId", SqlDbType.VarChar, 10, value);
+            }
+        }
+
+
+        public static class CabinetFiles
+        {
+            public static SqlParameter FileIdParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@FileId", SqlDbType.Int, value);
+            }
+
+            public static SqlParameter ClientIdParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@ClientId", SqlDbType.VarChar, 10, value);
+            }
+
+            public static SqlParameter QuickPickupIdParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@QuickPickupId", SqlDbType.VarChar, 20, value);
+            }
+        }
+
+        public static class IboArchive
+        {
+            public static SqlParameter ClientIdParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@ClientID", SqlDbType.VarChar, 50, value);
+            }
+        }
+
+        public static class IboHeader
+        {
+            public static SqlParameter BranchCodeParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@BranchCode", SqlDbType.VarChar, 50, value);
+            }
+
+            public static SqlParameter ClientIdParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@ClientID", SqlDbType.VarChar, 10, value);
+            }
+
+            public static SqlParameter CreatedByParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@CreatedBy", SqlDbType.VarChar, 255, value);
+            }
+
+            public static SqlParameter IboGuidParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@IboGuid", SqlDbType.VarChar, 33, value);
+            }
+
+            public static SqlParameter ImportFileIdParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@ImportFileId", SqlDbType.VarChar, 255, value);
+            }
+        }
+
+        public static class Screen
+        {
+            public static SqlParameter FieldNameParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@FieldName", SqlDbType.VarChar, 50, value);
+            }
+        }
+
+        public static class Session
+        {
+            public static SqlParameter UIdParamater(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@UId", SqlDbType.VarChar, 255, value);
+            }
+
+            public static SqlParameter UserNameParamater(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@UserName", SqlDbType.VarChar, 255, value);
+            }
+        }
+
+        public static class Users
+        {
+            public static SqlParameter UsernameParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@UserName", SqlDbType.VarChar, 75, value);
+            }
+        }
+
+        public static class WizGlobal
+        {
+            public static SqlParameter ClientOnlyParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@ClientOnly", SqlDbType.VarChar, 500, value);
+            }
+
+            public static SqlParameter InvCodeParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@InvCode", SqlDbType.VarChar, 10, value);
+            }
+
+            public static SqlParameter LoanProgramParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@LoanProg", SqlDbType.VarChar, 255, value);
+            }
+
+            public static SqlParameter SearchCriteriaParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@SearchCriteria", SqlDbType.VarChar, 255, value);
+            }
+
+            public static SqlParameter UserIdParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@UserId", SqlDbType.Int, value);
+            }
+
+            public static SqlParameter WizTypeParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@WizType", SqlDbType.VarChar, 1, value);
+            }
+        }
+
+        public static class WizLocal
+        {
+            public static SqlParameter AliasParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@Alias", SqlDbType.VarChar, 255, value);
+            }
+
+            public static SqlParameter ClientOnlyParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@ClientOnly", SqlDbType.VarChar, 500, value);
+            }
+
+            public static SqlParameter InvCodeParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@InvCode", SqlDbType.VarChar, 50, value);
+            }
+
+            public static SqlParameter SearchCriteriaParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@SearchCriteria", SqlDbType.VarChar, 255, value);
+            }
+
+            public static SqlParameter WizTypeParameter(string value)
+            {
+                return SqlMaster.Helper.CreateSqlParameter("@WizType", SqlDbType.VarChar, 1, value);
+            }
+        }
+    }
+}
